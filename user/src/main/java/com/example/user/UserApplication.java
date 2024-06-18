6ec6739a90d881f80c375012b7395e4fba75dd74
laurentiu <laurentiuandoni2001@gmail.com>
2024-06-18 22:35:49 +0300

    config eureka

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -2,9 +2,11 @@ package com.example.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RefreshScope
 public class UserApplication {
 	public static void main(String[] args) {
